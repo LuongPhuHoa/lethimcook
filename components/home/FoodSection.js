6aa1bedb57b1d439548e32b7cfdcabde0bb0eed1
LuongPhuHoa <36793008+LuongPhuHoa@users.noreply.github.com>
2023-07-31 23:14:56 +0700

    update layout

--- components/home/FoodSection.js
+++ components/home/FoodSection.js
@@ -8,8 +8,23 @@ function FoodSection() {
           <div className="m-6 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 max-h-full">
             <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">Vegetables</p>
             <div className="grid grid-cols-8 md:grid-cols-6 gap-4">
-              <div className="max-w-fit">
-                <a href="#" className="block max-w-fit p-6 py-0 bg-white border border-gray-200 rounded-3xl shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 ">
+              <div className="h-full">
+                <a href="#" className="block max-w-fit w-fit py-2 pt-3 px-3 bg-white border border-gray-200 rounded-3xl shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 ">
+                  <p className="font-normal text-gray-700 dark:text-gray-400">Onion</p>
+                </a>
+              </div>
+              <div className="h-full">
+                <a href="#" className="block max-w-fit w-fit py-2 pt-3 px-3 bg-white border border-gray-200 rounded-3xl shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 ">
+                  <p className="font-normal text-gray-700 dark:text-gray-400">Onion</p>
+                </a>
+              </div>
+              <div className="h-full">
+                <a href="#" className="block max-w-fit w-fit py-2 pt-3 px-3 bg-white border border-gray-200 rounded-3xl shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 ">
+                  <p className="font-normal text-gray-700 dark:text-gray-400">Onion</p>
+                </a>
+              </div>
+              <div className="h-full">
+                <a href="#" className="block max-w-fit w-fit py-2 pt-3 px-3 bg-white border border-gray-200 rounded-3xl shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 ">
                   <p className="font-normal text-gray-700 dark:text-gray-400">Onion</p>
                 </a>
               </div>
