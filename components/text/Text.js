0fe63a5558a4fb3877c00e59b1e2f89cf14e01f6
Restyled.io <commits@restyled.io>
2023-08-06 14:42:06 +0000

    Restyled by clang-format

--- components/text/Text.js
+++ components/text/Text.js
@@ -3,8 +3,9 @@ import React from "react";
 
 import classes from "./Text.module.scss";
 
-function Text({ children, className }) {
-  return <p className={clsx(classes.text, className)}>{children}</p>;
+function Text({children, className}) {
+  return <p className = {clsx(classes.text, className)}>{children} <
+         /p>;
 }
 
 export default Text;
--- pages/meals/index.js
+++ pages/meals/index.js
@@ -3,11 +3,9 @@ import React from "react";
 import classes from "./meals.module.scss";
 
 function index() {
-  return (
-    <div className={classes.pageWrapper}>
-      <div className={classes.topContainer}>Hi</div>
-    </div>
-  );
+  return (<div className = {classes.pageWrapper}>
+          <div className = {classes.topContainer}>Hi</div>
+    </div>);
 }
 
 export default index;
